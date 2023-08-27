0ce2e4ede3b239e99c9799d74a259215c3d02937
Sagar Neeraj <98794464+sagar598@users.noreply.github.com>
2023-08-27 13:59:46 +0000

    data types and some basics

---
+++ basics/dataTypesStictmode.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let age = 30;       // Number
+let name = "John";  // String
+let isAdmin = true; // Boolean
+let user = null;    // Null
+let person;         // Undefined
+
+
+console.log( typeof age)
+
+
+const accountID=1234;
+let accountEmail="mrsagar850@gmail.com";
+var accountPassword=7400585003;
+var accountCity="jaipur";
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
---
+++ basics/variableBasics.js
@@ -0,0 +1,11 @@
+const accountID=1234;
+let accountEmail="mrsagar850@gmail.com";
+var accountPassword=7400585003;
+var accountCity="jaipur";
+
+
+console.log(accountCity);
+
+console.table([accountID,accountEmail,accountPassword,accountCity, accountstate]);
+
+
